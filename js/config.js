90ac1cb4ea64962b88fab6cc49129e70667f7928
cathyw-3 <1436347574@qq.com>
2022-04-02 07:08:45 -0700

    wyc0402-v21

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ugxXCZ1N6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1vf2v6qj5lidn4spko0ppmmbl7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_L9iZMciML', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5q3pdhjbfoht0a50f4qderrh5i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://avisu1o5d0.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
