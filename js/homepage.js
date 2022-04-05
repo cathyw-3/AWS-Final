49e167d3002fb95693b69a2c7861a56eb3017173
cathyw-3 <1436347574@qq.com>
2022-04-04 20:06:28 -0700

    wyc0405-v11

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,45 @@
 var len = 3;
+var strs = [
+ {
+ "id":123,
+ "pwd":456
+ },
+ {
+ "id":456,
+ "pwd":123
+ },
+ {
+ "id":4681,
+ "pwd":9815
+ },
+ {
+ "id":471,
+ "pwd":4981
+ },
+ {
+ "id":1234789,
+ "pwd":156
+ },
+ {
+ "id":789,
+ "pwd":158
+ },
+ {
+ "id":"vgwh",
+ "pwd":"vgsya"
+ },
+ {
+ "id":1,
+ "pwd":"tcv"
+ },
+ {
+ "id":256,
+ "pwd":"vtc0"
+ }
+];
 
-function limits(data){
-	var countLim = Math.ceil(data.length/len);
+function limits(){
+	var countLim = Math.ceil(strs.length/len);
 	document.getElementById("inputs").innerHTML='';
 	for(var i=1;i<=countLim;i++){
  		document.getElementById("inputs").innerHTML+='<input type="button" value="'+i+'" onclick="limitinput(this)"/>';
@@ -9,22 +47,22 @@ function limits(data){
 }
 
 var choose = 0;
-function limitinput(data, ids){
+function limitinput(ids){
 	choose = ids.value;
- 	tablestr(data, choose);
- 	limits(data);
+ 	tablestr(choose);
+ 	limits();
   	ids.style.backgroundColor='red';
  	liminputcolor(choose);
 }
 
-function tablestr(data, num){
+function tablestr(num){
 	var num1 = (num-1)*len;
 	var num2 = num*len;
 	document.getElementById("table").innerHTML='';
 	for(var i=num1;i<num2;i++){
 		var str = '';
-		str+='<td>'+data[i].date.N+'</td>';
-		str+='<td>'+data[i].message.S+'</td>';
+		str+='<td>'+strs[i].id+'</td>';
+		str+='<td>'+strs[i].pwd+'</td>';
 		document.getElementById("table").innerHTML+='<tr>'+str+'</tr>';
  	}
 }
