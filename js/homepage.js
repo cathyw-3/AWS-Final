9cf401c56469b32f69fd5a3b2b3c452ca5a9a567
cathyw-3 <1436347574@qq.com>
2022-04-05 21:26:34 -0700

    wyc0406-v4

--- js/homepage.js
+++ js/homepage.js
@@ -33,7 +33,7 @@ function tablestr(num){
 		var str = '';
 		str+='<td>'+strs[i].date.N+'</td>';
 		str+='<td>'+strs[i].message.S+'</td>';
-		str+='<td>'+'<input type="button" value="love" onclick="handleLove({0})"/>'.format(strs[i].date.N)+'</td>';
+		str+='<td>'+'<input type="button" value="love"/>'+'</td>';
 		str_arr[i-num1] = str;
  	}
 	document.getElementById("table1").innerHTML+='<tr>'+str_arr[0]+'</tr>';
