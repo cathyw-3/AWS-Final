3d86aca153131a7e9317357457f4d15aeac62890
cathyw-3 <1436347574@qq.com>
2022-04-08 07:30:49 -0700

    wyc0408-v22

--- js/homepage.js
+++ js/homepage.js
@@ -53,6 +53,7 @@ function handleLove(userid) {
 	alert(userid);
 }
 
+const AWS = require('aws-sdk');
 AWS.config.update({
       region: "us-east-1",
       endpoint: 'https://dynamodb.us-east-1.amazonaws.com',
