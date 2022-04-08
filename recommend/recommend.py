97b184e79ef9b0ec578a35eb29cc893e7486a1cd
Auk <quinnxu19@foxmail.com>
2022-04-08 19:01:58 +0800

    'XuYuan0408-v3'

--- recommend/recommend.py
+++ recommend/recommend.py
@@ -2,12 +2,10 @@ import json
 from User import User
 from datetime import datetime, timezone, timedelta
 from DynamoDBService import DynamoDBService
-
+import urllib
 
 import numpy as np
 import pandas as pd
-
-http = urllib3.PoolManager()
 
 
 def get_latent_factors(records):
@@ -37,26 +35,32 @@ def recommend_top_k(p, Q, k):
 
 
 def lambda_handler(event, context):
-    print(event)
-    # TODO
+    # get user ID
+    userID = event.get('userid', '0')
 
-    # 1 & 2 Extract relevant metadata including S3URL out of input event
-    object_get_context = event["getObjectContext"]
-    request_route = object_get_context["outputRoute"]
-    request_token = object_get_context["outputToken"]
-    s3_url = object_get_context["inputS3Url"]
-    userID = object_get_context['userID']
-
-    # 3 - Download S3 File
-    response = http.request('GET', s3_url)
-
-    original_object = response.data.decode('utf-8')
-    as_list = json.loads(original_object)
+    # # 1 & 2 Extract relevant metadata including S3URL out of input event
+    # object_get_context = event["getObjectContext"]
+    # request_route = object_get_context["outputRoute"]
+    # request_token = object_get_context["outputToken"]
+    # s3_url = object_get_context["inputS3Url"]
+    #
+    # # 3 - Download S3 File
+    # response = http.request('GET', s3_url)
+    #
+    # original_object = response.data.decode('utf-8')
+    # as_list = json.loads(original_object)
+    # P, Q = get_latent_factors(as_list)
 
     # build the latent factors dictionary
     # P = get_latent_factors('user_latent_factor.txt')
     # Q = get_latent_factors('item_latent_factor.txt')
-    P, Q = get_latent_factors(as_list)
+
+    # get S3 bucket
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    response = s3.get_object(Bucket=bucket, Key=key)
+    doc = json.loads(response)
+    P, Q = get_latent_factors(doc)
 
     # get the user latent factor
     if userID in P.keys():
@@ -70,5 +74,5 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(recommend_list)
     }
