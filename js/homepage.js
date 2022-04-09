594bb9864ff53b29329e1e185fed6df9ffcdcb6f
cathyw-3 <1436347574@qq.com>
2022-04-09 00:22:01 -0700

    wyc0409-v34

--- js/homepage.js
+++ js/homepage.js
@@ -15,11 +15,27 @@ function limits(){
 	}
 }
 
+function limitshistory(){
+	var countLim = Math.ceil(strs.length/len);
+	document.getElementById("inputs").innerHTML='';
+	for(var i=1;i<=countLim;i++){
+ 		document.getElementById("inputs").innerHTML+='<input type="button" value="'+i+'" onclick="limithistoryinput(this)"/>';
+	}
+}
+
 var choose = 0;
 function limitinput(ids){
 	choose = ids.value;
  	tablestr(choose);
  	limits();
+  	ids.style.backgroundColor='red';
+ 	liminputcolor(choose);
+}
+
+function limithistoryinput(ids){
+	choose = ids.value;
+ 	tablehistory(choose);
+ 	limitshistory();
   	ids.style.backgroundColor='red';
  	liminputcolor(choose);
 }
@@ -38,12 +54,31 @@ function tablestr(num){
 			str+='<td>'+strs[i].flatid+'</td>';
 			str+='<td>'+strs[i].clike+'</td>';
 			var temp1 = "<input type=\"button\" value=\"love\" onclick='handleLove(\""+user_id+"\",\""+flat_id+"\")'/>";
-			alert(temp1);
 			str+='<td>'+temp1+'</td>';
 			str_arr[i-num1] = str;
 		}
  	}
 	var table_arr = ["table1", "table2", "table3"];
+	for (var j = 0; j < str_arr.length; ++j) {
+		document.getElementById(table_arr[j]).innerHTML+='<tr>'+str_arr[j]+'</tr>';
+	}
+}
+
+function tablehistory(num){
+	var str_arr = []
+	var num1 = (num-1)*len;
+	var num2 = num*len;
+	document.getElementById("history1").innerHTML='';
+	document.getElementById("history2").innerHTML='';
+	document.getElementById("history3").innerHTML='';
+	for(var i=num1;i<num2;i++){
+		if (i < strs.length) {
+			var str = '';
+			str+='<td>'+strs[i]+'</td>';
+			str_arr[i-num1] = str;
+		}
+ 	}
+	var table_arr = ["history1", "history2", "history3"];
 	for (var j = 0; j < str_arr.length; ++j) {
 		document.getElementById(table_arr[j]).innerHTML+='<tr>'+str_arr[j]+'</tr>';
 	}
